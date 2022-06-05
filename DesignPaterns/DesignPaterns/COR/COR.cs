13451d8eeae9df684f4ce15ba00a93a5230c7d1e
zlatkohajdarevic laptop <zlatkohajdarevic@gmail.com>
2022-06-05 12:11:38 +0200

    init

---
+++ DesignPaterns/DesignPaterns/COR/COR.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPaterns.COR
+{
+    public class Creature
+    {
+        public string Name;
+        public int Attack, Defense;
+
+
+        public Creature(string name, int attack, int defense)
+        {
+            Name = name ?? throw new ArgumentNullException(paramName: nameof(name));
+            Attack = attack;
+            Defense = defense;
+        }
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Attack)}: {Attack}, {nameof(Defense)}: {Defense},";
+        }
+
+
+    }
+
+    public class CreateModifier
+    {
+        protected Creature creature;
+        protected CreateModifier next; // linked List
+
+        public CreateModifier(Creature creature)
+        {
+            this.creature = creature;
+        }
+        public void Add(CreateModifier cm)
+        {
+            if (next != null) next.Add(cm);
+            else next = cm;
+        }
+        public virtual void Handle() => next?.Handle();
+    }
+
+    public class DoubleAttackModifier : CreateModifier
+    {
+        public DoubleAttackModifier(Creature creature) : base(creature)
+        {
+        }
+        public override void Handle()
+        {
+            Console.WriteLine($"Doubling {creature.Name}`s attack ");
+            creature.Attack *= 2;
+            base.Handle();
+        }
+    }
+
+    public class DoublDefenseModifier : CreateModifier
+    {
+        public DoublDefenseModifier(Creature creature) : base(creature)
+        {
+        }
+        public override void Handle()
+        {
+            Console.WriteLine($"Doubling {creature.Name}`s defense ");
+            creature.Defense += 3;
+            base.Handle();
+        }
+    }
+}
---
+++ DesignPaterns/DesignPaterns/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using DesignPaterns.COR;
+
+namespace DesignPaterns.Start
+{
+
+    public class Demo
+    {
+        public static void Main()
+        {
+            var goblin = new Creature("Goblin", 2, 2);
+            Console.WriteLine(goblin);
+            var root = new CreateModifier(goblin);
+            Console.WriteLine("Doubling");
+            root.Add(new DoubleAttackModifier(goblin));
+            root.Add(new DoublDefenseModifier(goblin));
+            root.Handle();
+
+            Console.WriteLine(goblin);
+
+
+
+        }
+    }
+}
+
